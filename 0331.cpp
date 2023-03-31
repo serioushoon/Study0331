cce07c3c981ea5d0adbecb294e82ea540dc5a3f3
serioushoon <enisy21@gmail.com>
2023-03-31 17:26:07 +0900

    플레이어 추가

--- 0331.cpp
+++ 0331.cpp
@@ -58,17 +58,24 @@ int main()
 	{1,1,1,1,1,1,1,1,1,1}
 	};
 
+	int PlayerX = 1;
+	int PlayerY = 1;
+
 	for (int Y = 0; Y < 10; ++Y)
 	{
 		for (int X = 0; X < 10; ++X)
 		{
-			if (Map[Y][X] == 1)
+			if (PlayerX == X && PlayerY == Y)
+			{
+				cout << "P";
+			}
+			else if (Map[Y][X] == 1)
 			{
 				cout << "#";
 			}
 			else if (Map[Y][X] == 0);
 			{
-				cout << " ";
+				cout << "  ";
 			}
 
 		}
